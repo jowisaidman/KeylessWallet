16aac05de8fa9cebd204f8a5bf1a6a36abc54962
jowisaidman <joelsaidman1@gmail.com>
2024-08-10 17:58:49 -0300

    small changes

--- src/scripts/requestMetamask.ts
+++ src/scripts/requestMetamask.ts
@@ -4,20 +4,20 @@
             const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
             console.log('Connected account:', accounts[0]);
             
-            const toAddress = "0x16CC7Bb7ae4D6f4A6EeA7dcE3F1492E4b70c5BB1";
-            const value = 0.001;
+            const hexNonce = '0x' + parseInt("1002", 10).toString(16); 
 
             const message = JSON.stringify({
-                "type": 2,
-                "chainId": 11155111,
-                "maxPriorityFeePerGas": "882358428",
-                "maxFeePerGas": "42874510220",
-                "gasLimit": 22000,
-                "to": toAddress,
-                "value": `${value}`,
-                "data":"0x",
-                "accessList": []
-            });
+                type: 2,
+                chainId: 11155111,
+                maxPriorityFeePerGas: '0x3495d2cc',
+                maxFeePerGas: '0x9fc5b6e3c',
+                gasLimit: '0x55f0',
+                to: '0x16CC7Bb7ae4D6f4A6EeA7dcE3F1492E4b70c5BB1',
+                value: '0x38d7ea4c68000', // 0.001 ETH in wei, converted to hex
+                data: '0x',
+                accessList: [],
+                nonce: hexNonce
+              });
 
             console.log("wating for personal_sign");
             const signedMessage = await window.ethereum.request({
