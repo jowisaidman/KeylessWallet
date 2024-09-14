3818a2ef17f0785c8084ba8c561de26aadd38836
Nicolas Antinori <nicolas_antinori@hotmail.com>
2024-09-13 22:03:31 -0300

    refactoring 4

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -17,19 +17,24 @@ async function injectExtensionScript(url: string) {
   }
 }
 
+// This listener is the nexus between the injected script in the DOM and the popup, because it has
+// access to both chrome.runtime and window.dispatchEvent|addEventListener APIs.
+//
+// To communicate with the background script and the popup it uses chrome.runtime API.
+// To communicate with the injected script in the dom it uses window.dispatchEvent|addEventListener
 window.addEventListener(
   "message",
   (event: CustomEventInit<Command>) => {
     const command: any = event.detail;
     console.log("from content script", JSON.stringify(command));
     switch (command.type) {
+      // Rpc calls that need to open the popup to do some action
       case RpcCall.EthRequestAccounts:
       case RpcCall.EthSendTranasaction:
         chrome.runtime.sendMessage(
           new Command(BackgroundCommand.OpenPopup),
           (response) => {
             console.log("open-popup response", response);
-
             if (response.success) {
               sendMessageToExtension(command).then((extensionResponse) => {
                 const responseEvent = new CustomEvent(command.id, {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -35,7 +35,7 @@ export function dispatchEvent(command: Command): Promise<unknown> {
 // Sends message to extension
 // This function uses the chrome.runtime api to send a message to the popup.
 // Returns a promise that encapsulates the response from the popup.
-export function sendMessageToExtension(event: CustomEvent): Promise<unknown> {
+export function sendMessageToExtension(event: CustomEventInit<Command>): Promise<unknown> {
   return new Promise((resolve, reject) => {
     console.log("sending", event);
     chrome.runtime.sendMessage(event, (response) => {
